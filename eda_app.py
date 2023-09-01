1771d8ce89e23d51b94785a94a9d9206058f7fa8
Kim Eun Chae <kimec995@gmail.com>
2023-09-01 10:38:18 +0900

    add 2 pages

---
+++ eda_app.py
@@ -0,0 +1,12 @@
+# -*- coding utf-8 -*-
+
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sns
+
+def run_eda_app():
+    st.subheader("탐색적 자료분석 페이지")
+    st.markdown("---")
+    st.subheader("잘 되는듯?")
---
+++ ml_app.py
@@ -0,0 +1,11 @@
+# -*- coding utf-8 -*-
+
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sns
+
+def run_ml_app():
+    st.subheader("머신러닝 페이지")
+    st.subheader("잘 되는듯?")
