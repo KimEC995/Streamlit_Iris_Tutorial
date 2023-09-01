83be84eaa11c5247a12deeac21095a44bb30ce2a
Kim Eun Chae <kimec995@gmail.com>
2023-09-01 10:20:09 +0900

    edit

--- app.py
+++ app.py
@@ -4,20 +4,19 @@ import streamlit as st
 
 
 def main():
-    st.markdown("# Hello World")
 
-    menu=["Home", "탐색적 자료 분석", "머신러닝", "About"]
-    choice = st.sidebar.selesctbox("메뉴",menu)
+    st.markdown("Hello World")
+    menu = ["Home", "탐색적 자료 분석", "머신러닝", "About"]
+    choice = st.sidebar.selectbox("메뉴", menu)
 
     if choice == "Home":
         st.subheader("Home")
     elif choice == "탐색적 자료 분석":
-        st.subheader("탐색적 자료 분석")
+        run_eda_app()
     elif choice == "머신러닝":
-        st.subheader("머신러닝")
+        run_ml_app()
     else:
-        pass
-    
+        st.subheader("About")
 
 if __name__=="__main__":
     main()
