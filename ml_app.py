15da4dbfac9434b32b97a6d8ca4b4923e1f446c2
Kim Eun Chae <kimec995@gmail.com>
2023-09-01 11:22:54 +0900

    Add Model

--- ml_app.py
+++ ml_app.py
@@ -1,12 +1,29 @@
 # -*- coding utf-8 -*-
 
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
-import seaborn as sns
+import joblib
+import os
+import numpy as np
 
 def run_ml_app():
-    st.subheader("머신러닝 페이지")
-    st.markdown("![BGI.jpg](data\BGI.jpg)")
-    st.subheader("잘 되는듯?")
+    st.subheader("Machine Learning Page")
+    
+    # layout
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("수치입력")
+        sepal_length = st.select_slider("Sepal Length", options = np.arange(1,11))
+        sepal_width = st.select_slider("Sepal Width", options = np.arange(1,11))
+        petal_length = st.select_slider("Petal Length", options = np.arange(1,11))
+        petal_width = st.select_slider("Petal Width", options = np.arange(1,11))
+
+        sample_list = [sepal_length, sepal_width, petal_length, petal_width]
+        st.write(sample_list)
+
+    with col2:
+        st.subheader("모델 결과를 확인하셈요")
+
+        # 모델 불러오기
+        model_file = "models/lgr_model_iris230901.pkl"
+        model = joblib.load(open(os.path.join(model_file), "rb"))
+        st.write(model)
