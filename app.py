8f3aabb6bf7c9f18a6004bfbda7e64d82d28a6df
Kim Eun Chae <kimec995@gmail.com>
2023-09-01 09:59:52 +0900

    add comend

--- app.py
+++ app.py
@@ -2,11 +2,21 @@
 
 import streamlit as st
 import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib 
+import scikit-learn
+import plotly
 
 def main():
 
     st.markdown("# Hello World")
     st.write(np.__version__)
+    st.write(pd.__version__)
+    st.write(sns.__version__)
+    st.write(matplotlib.__version__)
+    st.write(scikit-learn.__version__)
+    st.write(plotly.__version__)
 
 if __name__=="__main__":
     main()
