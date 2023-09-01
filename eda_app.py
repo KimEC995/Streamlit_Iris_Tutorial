84b1397d26fbdd08a67b883120305db7f098c861
Kim Eun Chae <kimec995@gmail.com>
2023-09-01 10:43:19 +0900

    add DataFrame

--- eda_app.py
+++ eda_app.py
@@ -9,4 +9,9 @@ import seaborn as sns
 def run_eda_app():
     st.subheader("탐색적 자료분석 페이지")
     st.markdown("---")
+    st.markdown("[마크다운추가하기](https://github.com/KimEC995)")
     st.subheader("잘 되는듯?")
+
+    iris_df = pd.read_csv("data/iris.csv")
+
+    st.dataframe(iris_df)
